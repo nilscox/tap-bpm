315cc466fe6e2d2b61952aee68e35b861f1a160c
nils <nilscox.dev@gmail.com>
2025-12-27 17:27:11 +0100

    remove the example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import { Counter } from './components/counter/Counter';
-
 export function App() {
-  return (
-    <div>
-      <h2>Redux Counter with softer components</h2>
-      <Counter />
-    </div>
-  );
+  return <></>;
 }
--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,3 @@
 import { configureSofterStore } from '@softer-components/redux-adapter';
 
-import { counterComponentDef } from './components/counter/counter.component.ts';
-
-export const store = configureSofterStore(counterComponentDef);
+export const store = configureSofterStore({});
